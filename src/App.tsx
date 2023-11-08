758dbc0a5f2e4417bd9902220ed7248e47aff8f4
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-11-08 13:26:26 -0300

    Created a reusable styled button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Button } from "./components/Button/Button";
 
 function App() {
-  return <Button onClick={() => console.log("press btn")}>Сохранить</Button>;
+  return (
+    <>
+      <Button onClick={() => console.log("press btn")}>Применить</Button>
+      <Button size="big" onClick={() => console.log("press btn")}>
+        Оформить
+      </Button>
+    </>
+  );
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,21 @@ import cn from "classnames";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  size?: "big" | "small";
 }
 
-export const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  size = "small",
+  ...props
+}) => {
   return (
     <button
-      className={cn(styles["button"], styles["accent"], className)}
+      className={cn(styles["button"], styles["accent"], className, {
+        [styles["small"]]: size === "small",
+        [styles["big"]]: size === "big",
+      })}
       {...props}
     >
       {children}
