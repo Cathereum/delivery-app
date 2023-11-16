3e608160b4553d426ed46587d8d3265fc83744f8
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-11-16 13:07:57 -0300

    added a React lazy-loading for "Menu" component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Cart } from "./pages/Cart/Cart";
 import { ProductPage } from "./pages/ProductPage/ProductPage.tsx";
 import { ErrorPage } from "./pages/ErrorPage/ErrorPage";
-import "./index.css";
 import { Layout } from "./layout/Layout/Layout.tsx";
-import { Menu } from "./pages/Menu/Menu.tsx";
 import axios from "axios";
 import { PREFIX } from "./helpers/API.ts";
+import "./index.css";
+
+const Menu = lazy(() => import("./pages/Menu/Menu"));
 
 const router = createBrowserRouter([
   {
@@ -17,7 +18,11 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Menu />,
+        element: (
+          <Suspense fallback={<>ИДЕТ ЗАГРУЗКА...</>}>
+            <Menu />
+          </Suspense>
+        ),
       },
       {
         path: "/cart",
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -7,7 +7,7 @@ import styles from "./Menu.module.css";
 import axios, { AxiosError } from "axios";
 import { MenuList } from "./MenuList/MenuList";
 
-export const Menu = () => {
+const Menu = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | undefined>(undefined);
@@ -44,3 +44,5 @@ export const Menu = () => {
     </>
   );
 };
+
+export default Menu;
