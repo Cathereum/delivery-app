9fec3d1c5f08080a83574a706e8e9d5c46640531
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-11-18 10:37:05 -0300

    getting Access token, handling "LoginForm" & error

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,24 +3,63 @@ import { Button } from "../../components/Button/Button";
 import { Headling } from "../../components/Headling/Headling";
 import styles from "./LoginPage.module.css";
 import { Input } from "../../components/Input/Input";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { PREFIX } from "../../helpers/API";
+
+export type LoginForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
 
 export const LoginPage = () => {
+  const [error, setError] = useState<string | null>(null);
+
   const formSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log(e);
+    setError(null);
+    const target = e.target as typeof e.target & LoginForm;
+    // const email = target.email.value;
+    // const password = target.password.value;
+    const { email, password } = target;
+    sendLogin(email.value, password.value);
   };
+
+  const sendLogin = async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post(`${PREFIX}/auth/login`, {
+        email,
+        password,
+      });
+      console.log(data);
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        setError(e.response?.data.message);
+      }
+    }
+  };
+
   return (
     <div className={styles["login"]}>
       <Headling className={styles["title"]}>Вход</Headling>
+      {error && <span className={styles["error-message"]}>{error}</span>}
       <form className={styles["form"]} onSubmit={formSubmit}>
         <div className={styles["field"]}>
-          <label htmlFor="emal">Ваш email</label>
-          <Input id="emal" type="email" placeholder="Email" />
+          <label htmlFor="email">Ваш email</label>
+          <Input id="email" name="email" type="email" placeholder="Email" />
         </div>
         <div className={styles["field"]}>
           <label htmlFor="password">Ваш пароль</label>
-          <Input id="password" type="password" placeholder="Пароль" />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Пароль"
+          />
         </div>
         <div className={styles["button-wrapper"]}>
           <Button className={styles["button"]} size={"big"}>
