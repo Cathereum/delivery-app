10013fb25d698f6e2aa649bbb59e837dd427aff2
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-11-21 15:51:49 -0300

    completed  style and logic for "RegisterPage"

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,3 +1,87 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
+import { Headling } from "../../components/Headling/Headling";
+import styles from "./Register.module.css";
+import { Input } from "../../components/Input/Input";
+import { FormEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+import { register, userActions } from "../../store/user.slice";
+
+export type RegisterForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+  name: {
+    value: string;
+  };
+};
+
 export const RegisterPage = () => {
-  return <div>RegisterPage Content</div>;
+  const dispatch = useDispatch<AppDispatch>();
+  const { jwt, registerErrorMessage } = useSelector(
+    (state: RootState) => state.user
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (jwt) {
+      navigate("/");
+    }
+  }, [jwt]);
+
+  const formSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    dispatch(userActions.clearRegisterError());
+    const target = e.target as typeof e.target & RegisterForm;
+    // const email = target.email.value;
+    // const password = target.password.value;
+    const { email, password, name } = target;
+    sendRegister(email.value, password.value, name.value);
+  };
+
+  const sendRegister = (email: string, password: string, name: string) => {
+    dispatch(register({ email, password, name }));
+  };
+
+  return (
+    <div className={styles["register"]}>
+      <Headling className={styles["title"]}>Вход</Headling>
+      {registerErrorMessage && (
+        <span className={styles["error-message"]}>{registerErrorMessage}</span>
+      )}
+      <form className={styles["form"]} onSubmit={formSubmit}>
+        <div className={styles["field"]}>
+          <label htmlFor="email">Ваш email</label>
+          <Input id="email" name="email" type="email" placeholder="Email" />
+        </div>
+        <div className={styles["field"]}>
+          <label htmlFor="password">Ваш пароль</label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Пароль"
+          />
+        </div>
+        <div className={styles["field"]}>
+          <label htmlFor="name">Ваше имя</label>
+          <Input id="name" name="name" type="text" placeholder="Имя" />
+        </div>
+        <div className={styles["button-wrapper"]}>
+          <Button className={styles["button"]} size={"big"}>
+            Зарегистрироваться
+          </Button>
+        </div>
+      </form>
+      <div className={styles["form-footer"]}>
+        <span>Есть аккаунт?</span>
+        <Link to={"/auth/login"}>Войти</Link>
+      </div>
+    </div>
+  );
 };
--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -15,6 +15,7 @@ export interface StorageDataProps {
 export interface UserState {
   jwt: string | null;
   loginErrorMessage?: string;
+  registerErrorMessage?: string;
   profile?: Profile;
 }
 
@@ -29,6 +30,24 @@ export const logIn = createAsyncThunk(
       const { data } = await axios.post<JwtToken>(`${PREFIX}/auth/login`, {
         email: params.email,
         password: params.password,
+      });
+      return data;
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        throw new Error(e.response?.data.message);
+      }
+    }
+  }
+);
+
+export const register = createAsyncThunk(
+  "user/register",
+  async (params: { email: string; password: string; name: string }) => {
+    try {
+      const { data } = await axios.post<JwtToken>(`${PREFIX}/auth/register`, {
+        email: params.email,
+        password: params.password,
+        name: params.name,
       });
       return data;
     } catch (e) {
@@ -60,6 +79,9 @@ export const userSlice = createSlice({
     clearLoginError: (state) => {
       state.loginErrorMessage = undefined;
     },
+    clearRegisterError: (state) => {
+      state.registerErrorMessage = undefined;
+    },
   },
   extraReducers(builder) {
     builder.addCase(logIn.fulfilled, (state, action) => {
@@ -70,6 +92,15 @@ export const userSlice = createSlice({
     });
     builder.addCase(logIn.rejected, (state, action) => {
       state.loginErrorMessage = action.error.message;
+    });
+    builder.addCase(register.fulfilled, (state, action) => {
+      if (!action.payload) {
+        return;
+      }
+      state.jwt = action.payload.access_token;
+    });
+    builder.addCase(register.rejected, (state, action) => {
+      state.registerErrorMessage = action.error.message;
     });
     builder.addCase(getProfile.fulfilled, (state, action) => {
       if (!action.payload) {
